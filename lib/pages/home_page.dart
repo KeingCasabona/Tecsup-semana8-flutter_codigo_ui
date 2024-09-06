52bac3784290bd965d2b51f69abf4f7b65690db5
KeingCasabona <keing.limache.casabona@gmail.com>
2024-09-06 18:14:14 -0500

    feat: Add header gradient

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,11 +13,23 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0XFF0A494E),
+                    Color(0xff05172a),
+                  ],
+                ),
+              ),
               padding: EdgeInsets.symmetric(horizontal: 20),
               //height: 300,
               width: double.infinity,
-              color: Colors.blue,
-              child: SafeArea(
+
+              child: Padding(
+                padding:
+                    EdgeInsets.only(top: MediaQuery.of(context).padding.top),
                 child: Column(
                   children: [
                     Row(
@@ -66,7 +78,56 @@ class HomePage extends StatelessWidget {
                           ),
                         ),
                       ),
-                    )
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.black54,
+                              ),
+                              hintText: 'Find parking place',
+                              hintStyle: GoogleFonts.montserrat(
+                                color: Colors.black54,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 12),
+                        Container(
+                          height: 54,
+                          width: 54,
+                          child: Icon(
+                            Icons.filter_alt_outlined,
+                            color: Colors.white,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xffFFb404),
+                                Color.fromARGB(255, 255, 200, 71),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20),
                   ],
                 ),
               ),
